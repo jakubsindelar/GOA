68e587e9036626d732d7ae0672688b4305baf2fc
Hatake Kakashi <fanatik@KAKASHI-DEB>
2013-11-04 07:13:06 +0100

    Upravena adresářová struktura a index.php

---
+++ www/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <title>Třída obchodní Akademie Gymnazia pro zrakově postižné - Praha-5 155 00</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=9" />
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/login.css">
+    <?php include("../config.php"); ?>
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <meta name="description" content="Třídní web Obchodní Akademie z Gymnásia pro zrakově postižné">
+    <meta name="author" content="Jakub Šindelář" />
+  </head>
+      <body>
+        <div id="body"> 
+            <header>
+              <a href="./" style="text-decoration:none;">Třída Obchodní Akademie</a>
+             </header>
+        <span id="sloupec-l">
+        <?php include("../sloupec-l.php"); ?>
+        </span>
+        <span id="sloupec-r">
+        <?php include("../sloupec-r.php"); ?>
+        </span>
+          <div id="zapati">
+           Copiright © 2012 © Coding by <a href="mailto:fanatik@heroesofmemory.eu">Fanatik</a> Design by <a href="mailto:gimbo@seznam.cz">Gimbo</a>
+          </div>
+        </div>
+      </body>
+</html>
