c85a28e06e2f432b663817ce5493203bd91dd252
jakubsindelar <github@heroesofmemory.eu>
2014-09-04 21:09:39 +0200

    odkazy migrace formulář

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140904162515) do
+ActiveRecord::Schema.define(version: 20140904185035) do
 
   create_table "subjects", force: true do |t|
     t.string   "name"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20140904162515) do
     t.string   "name"
     t.string   "surname"
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tests", force: true do |t|
+    t.string   "name"
+    t.integer  "subject_id"
+    t.date     "start_date"
+    t.string   "note"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
