f607b945dc2e52c3c396aa05cd38f260e6afa25f
jakubsindelar <github@heroesofmemory.eu>
2014-09-04 18:30:17 +0200

    propojení předmětů a učitelů, rozbalovací seznam učitelů, do předmětu přidána kolonka teacher_id

---
+++ db/migrate/20140904162515_add_teacher_id_to_subject.rb
@@ -0,0 +1,5 @@
+class AddTeacherIdToSubject < ActiveRecord::Migration
+  def change
+  add_column :subjects, :teacher_id, :integer 
+ end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140904155453) do
+ActiveRecord::Schema.define(version: 20140904162515) do
 
   create_table "subjects", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140904155453) do
     t.integer  "classroom"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "teacher_id"
   end
 
   create_table "teachers", force: true do |t|
