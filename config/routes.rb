eb67c6097e5ba7b6e1f0fa83db9daaad74ac6792
jakubsindelar <github@heroesofmemory.eu>
2014-09-14 01:02:43 +0200

    přidán rozvrh

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :subjects
 
   get 'home/index'
-
+  get 'home/rozvrh'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
