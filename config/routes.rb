93e2c10496892ff727bd5993ccd4975b85869769
jakubsindelar <github@heroesofmemory.eu>
2014-10-22 08:35:11 +0200

    Přidána termínová listina s textem Lorem Ipsum, upraven odkaz a routy

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   get 'home/index'
   get 'home/rozvrh'
+  get 'home/akce'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
