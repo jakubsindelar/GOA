52a2d15450ae3f6eb9860042221926dfc3ec254e
gimbo95 <gimbo@heroesofmemory.eu>
2014-10-23 17:39:34 +0200

    Překlad a úprava hlášek homeworks subjects teachers tests controllers. Úprava textu v layouts ontroller

--- app/controllers/homeworks_controller.rb
+++ app/controllers/homeworks_controller.rb
@@ -28,7 +28,7 @@ class HomeworksController < ApplicationController
 
     respond_to do |format|
       if @homework.save
-        format.html { redirect_to @homework, notice: 'Homework was successfully created.' }
+        format.html { redirect_to @homework, notice: 'Úkol byl úspěšně přidán.' }
         format.json { render :show, status: :created, location: @homework }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class HomeworksController < ApplicationController
   def update
     respond_to do |format|
       if @homework.update(homework_params)
-        format.html { redirect_to @homework, notice: 'Homework was successfully updated.' }
+        format.html { redirect_to @homework, notice: 'Úkol byl úspěšně upraven.' }
         format.json { render :show, status: :ok, location: @homework }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class HomeworksController < ApplicationController
   def destroy
     @homework.destroy
     respond_to do |format|
-      format.html { redirect_to homeworks_url, notice: 'Homework was successfully destroyed.' }
+      format.html { redirect_to homeworks_url, notice: 'Úkol byl úspěšně smazán.' }
       format.json { head :no_content }
     end
   end
